55cb74b0cf25049b27908f602280ab071b5e8f07
Yan Santos <yansantos.dev@gmail.com>
2021-08-16 15:09:11 -0300

    skill finalizada

--- src/index.js
+++ src/index.js
@@ -9,25 +9,39 @@ app.get("/", async (request, response) => {
   page.setDefaultNavigationTimeout(0);
   await page.goto("https://br.op.gg/champion/statistics");
   await page.click(
-    `div:nth-child(9) > a > div.champion-index__champion-item__name`
-  );
+    `div:nth-child(3) > a > div.champion-index__champion-item__name`
+  );// erro champ aphelios 
   //esperar alguns segundos 
-  await new Promise(resolve => setTimeout(() => resolve(), 10000, console.log("espere")) );
+  await new Promise(resolve => setTimeout(() => resolve(), 20000, console.log("espere")));
 
   const pageContent = await page.evaluate(() => {
-    let firtSkill = document.querySelector(
-      "tbody:nth-child(5) > tr > td.champion-overview__data > table > tbody > tr:nth-child(2) > td:nth-child(9) > span"
-    )
-   // let secondSkill = document.querySelector(
-     // "tbody:nth-child(5) > tr > td.champion-overview__data > table > tbody > tr:nth-child(2) > td:nth-child(13) > span"
-    //)
+    const infoChamp =
+    {
+      name: //document.querySelector(" div.champion-stats-header-info.champion-stats-header-info--tier-5 > h1 "
+        // ).innerText,,
+        "null",
+      skills: {
+        firtSkill: document.querySelector(
+          "tbody:nth-child(5) > tr > td.champion-overview__data > table > tbody > tr:nth-child(2) > td:nth-child(9) > span"
+        ).innerHTML,
+        secoundSkill: document.querySelector(
+          "tbody:nth-child(5) > tr > td.champion-overview__data > table > tbody > tr:nth-child(2) > td:nth-child(13) > span"
+        ).innerHTML,
+        thirdskill: document.querySelector(" tbody:nth-child(5) > tr > td.champion-overview__data > table > tbody > tr:nth-child(2) > td:nth-child(14)"
+        ).innerText,
+      },
+      starterItems : {
 
-    return firtSkill.innerHTML
+      }	
+    },
+
+    return infoChamp
 
   });
-  //console.log(pageContent);
+  console.log(pageContent);
 
   await browser.close();
+
   response.send({
     pageContent
   });
