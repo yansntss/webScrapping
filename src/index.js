5ff7b0657659ce88e4760db365bb3b2f060a0296
yansntss <yansantos.dev@gmail.com>
2021-08-16 23:06:30 -0300

    Selecionando o champ dinamicamente

--- src/index.js
+++ src/index.js
@@ -7,43 +7,54 @@ app.get("/", async (request, response) => {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   page.setDefaultNavigationTimeout(0);
-  await page.goto("https://br.op.gg/champion/statistics");
-  await page.click(
-    `div:nth-child(3) > a > div.champion-index__champion-item__name`
-  );// erro champ aphelios 
-  //esperar alguns segundos 
-  await new Promise(resolve => setTimeout(() => resolve(), 20000, console.log("espere")));
 
-  const pageContent = await page.evaluate(() => {
-    const infoChamp =
-    {
-      name: //document.querySelector(" div.champion-stats-header-info.champion-stats-header-info--tier-5 > h1 "
-        // ).innerText,,
-        "null",
-      skills: {
-        firtSkill: document.querySelector(
-          "tbody:nth-child(5) > tr > td.champion-overview__data > table > tbody > tr:nth-child(2) > td:nth-child(9) > span"
-        ).innerHTML,
-        secoundSkill: document.querySelector(
-          "tbody:nth-child(5) > tr > td.champion-overview__data > table > tbody > tr:nth-child(2) > td:nth-child(13) > span"
-        ).innerHTML,
-        thirdskill: document.querySelector(" tbody:nth-child(5) > tr > td.champion-overview__data > table > tbody > tr:nth-child(2) > td:nth-child(14)"
-        ).innerText,
-      },
-      starterItems : {
+  //const champList = 1;
+  //const count = 2;
+  var arrayChamps = [];
+  for (let i = 1; i < 50; i++) {
+    console.log("entrou no for");
+    await page.goto("https://br.op.gg/champion/statistics");
+    await page.click(
+      `div:nth-child(${[i]}) > a > div.champion-index__champion-item__name`
+    ); // erro champ aphelios
+    //esperar alguns segundos
+    await new Promise((resolve) =>
+      setTimeout(() => resolve(), 20000, console.log("espere"))
+    );
 
-      }	
-    },
+    const pageContent = await page.evaluate(() => {
+      console.log("entrou no pageContent");
+      const infoChamp = {
+        name: document.querySelector("div.champion-box-header__title > h4")
+          .innerHTML,
 
-    return infoChamp
+        //ordem de up das skills
+        skills: {
+          firtSkill: document.querySelector(
+            "tbody:nth-child(5) > tr > td.champion-overview__data > table > tbody > tr:nth-child(2) > td:nth-child(9) > span"
+          ).innerHTML,
+          secoundSkill: document.querySelector(
+            "tbody:nth-child(5) > tr > td.champion-overview__data > table > tbody > tr:nth-child(2) > td:nth-child(13) > span"
+          ).innerHTML,
+          thirdskill: document.querySelector(
+            " tbody:nth-child(5) > tr > td.champion-overview__data > table > tbody > tr:nth-child(2) > td:nth-child(14)"
+          ).innerText,
+        },
+        //items inicial
+        // starterItems : {
 
-  });
-  console.log(pageContent);
+        // }
+      };
 
-  await browser.close();
+      return infoChamp;
+    });
 
+    //await browser.close();
+
+    arrayChamps.push(pageContent);
+  }
   response.send({
-    pageContent
+    arrayChamps,
   });
 
   //console.log("entrou");
